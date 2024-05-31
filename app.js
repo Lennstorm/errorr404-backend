52704c5d3259106957a35f5e3c9e8aa1f46a6213
Lennstorm <4824136@gmail.com>
2024-05-31 13:59:16 +0200

    created orders functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import customerRouter from "./src/routes/customers.js";
 import productRouter from "./src/routes/products.js";
 import aboutRouter from "./src/routes/about.js";
 import cartRouter from "./src/routes/cart.js";
+import ordersRouter from "./src/routes/orders.js";
 import cors from "cors";
 import { initializeDatabase } from "./src/services/product.js";
 
@@ -17,6 +18,7 @@ app.use("/api/customers", customerRouter);
 app.use("/api/products", productRouter);
 app.use("/about", aboutRouter);
 app.use('/cart', cartRouter);
+app.use("/orders", ordersRouter);
 
 // Initialize the database with default data if empty, then start the server
 const PORT = process.env.PORT || 3000;
